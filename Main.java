f38a18926c8b396af6189ff29833c7f420832a6e
RobertTronhage <143345075+RobertTronhage@users.noreply.github.com>
2023-10-02 13:20:28 +0200

    Revert "started working on removePaper method"

--- Main.java
+++ Main.java
@@ -1,18 +1,45 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    public static void removePaper(){
+public class Main {
+    static Scanner input = new Scanner(System.in);
+    static ArrayList<Newspaper> allNewspapers = new ArrayList<Newspaper>();
+    public static void main(String[] args) {
+        boolean exitProgram = false;
+        do {
+            System.out.println("Välkommen till tidnings adminsitrationen!");
+            System.out.println("0. Avsluta program");
+            System.out.println("1. Lägg till tidning");
+            System.out.println("2. visa alla tidningar");
+            //System.out.println("Visa alla tidningar");
+            System.out.println("Ditt val: ");
+            int userChoice = input.nextInt();
+            input.nextLine();
 
-        System.out.println("ange ID på den tidning du vill ta bort");
+            switch (userChoice){
+                case 0 -> exitProgram = true;
+                case 1 -> addPaper();
+                case 2 -> showAllPapers();
 
-        int foundId = input.nextInt();
-        input.nextLine()//consumes rest of line.
-
-        for (int i = 0; i < allNewspapers.size();i++){
-            if (allNewspapers.get(i).getPaperId()==foundId){
-                allNewspapers.remove(i);
-                break;
             }
-        }
+        } while (!exitProgram);
+
+    }
+    public static void addPaper(){
+
+            System.out.print("Vad ska tidningen ha för namn: ");
+            String newName = input.nextLine();
+            Newspaper n = new Newspaper(newName);
+            allNewspapers.add(n);
+            System.out.println("du har lagt till " + n);
+
 
 
     }
-    
+    public static void showAllPapers(){
+        for (Newspaper n:allNewspapers){
+            System.out.println(n);
+        }
+    }
+
+}
