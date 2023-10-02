6543b939c388ead6f58b2c1a2612fb82123aca87
Husade <119759512+Gazer174@users.noreply.github.com>
2023-10-02 10:32:28 +0200

    Add files via upload
    lagt till tidning!

--- Main.java
+++ Main.java
@@ -1,6 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+    static Scanner input = new Scanner(System.in);
+    static ArrayList<Newspaper> allNewspapers = new ArrayList<Newspaper>();
     public static void main(String[] args) {
+        System.out.println("Välkommen till tidnings adminsitrationen!");
+        System.out.println("1. Lägg till tidning");
+        //System.out.println("Visa alla tidningar");
+        System.out.println("Ditt val: ");
+        int userChoice = input.nextInt();
+        input.nextLine();
+
+        switch (userChoice){
+            case 1 -> addPaper();
+        }
 
     }
+    public static void addPaper(){
+        int userChoice = 0;
+        do {
+            System.out.print("Vad ska tidningen ha för namn: ");
+            String newName = input.nextLine();
+            Newspaper n = new Newspaper(newName);
+            allNewspapers.add(n);
+        } while (userChoice!=0);
+
+
+    }
+
 }
--- Newspaper.java
+++ Newspaper.java
@@ -4,8 +4,80 @@ import java.util.Date;
 public class Newspaper {
     private String paperName;
     private int paperId;
+    private Date date;
+    private int pages;
+    private double price;
+    private String articles;
 
-    private Date
+    public Newspaper(String paperName, int paperId, Date date, int pages, double price, String articles) {
+        this.paperName = paperName;
+        this.paperId = paperId;
+        this.date = date;
+        this.pages = pages;
+        this.price = price;
+        this.articles = articles;
+    }
+    public Newspaper(String paperName){
+        this.paperName = paperName;
+    }
 
+    public String getPaperName() {
+        return paperName;
+    }
 
+    public void setPaperName(String paperName) {
+        this.paperName = paperName;
+    }
+
+    public int getPaperId() {
+        return paperId;
+    }
+
+    public void setPaperId(int paperId) {
+        this.paperId = paperId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getArticles() {
+        return articles;
+    }
+
+    public void setArticles(String articles) {
+        this.articles = articles;
+    }
+
+    @Override
+    public String toString() {
+        return "Newspaper{" +
+                "paperName='" + paperName + '\'' +
+                ", paperId=" + paperId +
+                ", date=" + date +
+                ", pages=" + pages +
+                ", price=" + price +
+                ", articles='" + articles + '\'' +
+                '}';
+    }
 }
